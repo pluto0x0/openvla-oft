e2e14745a7eed5cb2c2329a49a33ab4ae76cc434
YingZifan-Docker <yzzzf0@gmail.com>
2025-06-24 15:57:42 +0800

    fix file structure

---
+++ src/openvla_oft/__init__.py
@@ -0,0 +1,4 @@
+from .myutils.deps import GenerateConfig
+
+from . import prismatic
+from . import experiments
---
+++ src/openvla_oft/myutils/deps.py
@@ -0,0 +1,64 @@
+from dataclasses import dataclass
+from enum import Enum
+from pathlib import Path
+from typing import Optional, Union
+
+# Define task suite constants
+class TaskSuite(str, Enum):
+    LIBERO_SPATIAL = "libero_spatial"
+    LIBERO_OBJECT = "libero_object"
+    LIBERO_GOAL = "libero_goal"
+    LIBERO_10 = "libero_10"
+    LIBERO_90 = "libero_90"
+
+@dataclass
+class GenerateConfig:
+    # fmt: off
+
+    #################################################################################################################
+    # Model-specific parameters
+    #################################################################################################################
+    model_family: str = "openvla"                    # Model family
+    pretrained_checkpoint: Union[str, Path] = ""     # Pretrained checkpoint path
+
+    use_l1_regression: bool = True                   # If True, uses continuous action head with L1 regression objective
+    use_diffusion: bool = False                      # If True, uses continuous action head with diffusion modeling objective (DDIM)
+    num_diffusion_steps_train: int = 50              # (When `diffusion==True`) Number of diffusion steps used for training
+    num_diffusion_steps_inference: int = 50          # (When `diffusion==True`) Number of diffusion steps used for inference
+    use_film: bool = False                           # If True, uses FiLM to infuse language inputs into visual features
+    num_images_in_input: int = 2                     # Number of images in the VLA input (default: 1)
+    use_proprio: bool = True                         # Whether to include proprio state in input
+
+    center_crop: bool = True                         # Center crop? (if trained w/ random crop image aug)
+    num_open_loop_steps: int = 8                     # Number of actions to execute open-loop before requerying policy
+
+    lora_rank: int = 32                              # Rank of LoRA weight matrix (MAKE SURE THIS MATCHES TRAINING!)
+
+    unnorm_key: Union[str, Path] = ""                # Action un-normalization key
+
+    load_in_8bit: bool = False                       # (For OpenVLA only) Load with 8-bit quantization
+    load_in_4bit: bool = False                       # (For OpenVLA only) Load with 4-bit quantization
+
+    #################################################################################################################
+    # LIBERO environment-specific parameters
+    #################################################################################################################
+    task_suite_name: str = TaskSuite.LIBERO_SPATIAL  # Task suite
+    num_steps_wait: int = 10                         # Number of steps to wait for objects to stabilize in sim
+    num_trials_per_task: int = 50                    # Number of rollouts per task
+    initial_states_path: str = "DEFAULT"             # "DEFAULT", or path to initial states JSON file
+    env_img_res: int = 256                           # Resolution for environment images (not policy input resolution)
+
+    #################################################################################################################
+    # Utils
+    #################################################################################################################
+    run_id_note: Optional[str] = None                # Extra note to add to end of run ID for logging
+    local_log_dir: str = "./experiments/logs"        # Local directory for eval logs
+
+    use_wandb: bool = False                          # Whether to also log results in Weights & Biases
+    wandb_entity: str = "your-wandb-entity"          # Name of WandB entity
+    wandb_project: str = "your-wandb-project"        # Name of WandB project
+
+    seed: int = 7                                    # Random Seed (for reproducibility)
+
+    # fmt: on
+
